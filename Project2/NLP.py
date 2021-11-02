a8231f4c1ae61d6caa965209bf06413b080534c5
EveZhang <55336328+YiqinZhang@users.noreply.github.com>
2021-11-02 15:37:03 -0400

    Update NLP.py

--- Project2/NLP.py
+++ Project2/NLP.py
@@ -1,11 +1,10 @@
 import os
-from google.cloud import language
-from google.cloud.language import enums
-from google.cloud.language import types
+from google.cloud import language_v1
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=r"path to jason file"
 
-client = language.LanguageServiceClient()
-filelist = os.listdir("test_file")  # make sure no DS.Store file in the folder
-# print(filelist)
+client = language_v1.LanguageServiceClient()
+filelist = os.listdir("result")  # make sure no DS.Store file in the folder
+
 filenum = len(filelist)
 sequence = {}
 M = 0
@@ -13,12 +12,16 @@ for i in range(0, filenum):
     filename = filelist[i]
     review_file = os.path.join("result", filename)
     name = os.path.splitext(filename)[0]
+    
     with open(review_file, "r") as review_file:
         # Instantiates a plain text document.
         content = review_file.read()
 
-    document = types.Document(content=content, type=enums.Document.Type.PLAIN_TEXT)
-    annotations = client.analyze_sentiment(document=document)
+    document = language_v1.Document(content=content, type_=language_v1.Document.Type.PLAIN_TEXT)
+    # annotations = client.analyze_sentiment(document=document)
+    encoding_type = language_v1.EncodingType.UTF8
+    annotations = client.analyze_sentiment(request = {'document': document, 'encoding_type': encoding_type})
+    # print(annotations)
     # Print the results
     score = annotations.document_sentiment.score
     magnitude = annotations.document_sentiment.magnitude
@@ -37,9 +40,7 @@ for i in range(0, filenum):
 
 sortedsequence = sorted(sequence.items(), key=lambda x: x[1], reverse=True)
 b = [i[0] for i in sortedsequence]
-# a =[i[1] for i in sortedsequence]
 result = open("result.txt", "a")
 result.write("Top 10 US cities most welcome on Twitter for a short trip" + "\n")
 for n in range(0, 10):
     result.write(str(n + 1) + "." + b[n] + "\n")
-    # result.write (str(n+1)+'.'+b[n]+' '+str(a[n])+'\n')
